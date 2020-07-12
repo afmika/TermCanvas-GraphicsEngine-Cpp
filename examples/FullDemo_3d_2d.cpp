9bbca3f6949437694522402a971c037a8923cdad
afmika <afmichael73@gmail.com>
2020-07-12 23:13:50 +0300

    New demos

--- examples/FullDemo_3d_2d.cpp
+++ examples/FullDemo_3d_2d.cpp
@@ -5,8 +5,9 @@
 
 #include "TermCanvasInc.h"
 
-const int width  = 30;
-const int height = 24;
+const int width  = 80;
+const int height = 35;
+int deltax = 1;
 
 void animateFace(char axis, double angle, int scale, TermCanvas& canvas, Graphics::Figure::Shape& cube ) {
     Graphics::Figure::Shape to_draw = Graphics::Transf::scale(cube, scale);
@@ -16,11 +17,15 @@ void animateFace(char axis, double angle, int scale, TermCanvas& canvas, Graphic
     cube = Graphics::Transf::rotate(cube, angle, axis);
 }
 
+int func_def (int x) {
+    return 5 * std::sin(x + deltax);
+}
+
 int main() {
     TermCanvas canvas(width, height);
     canvas.setColorPalette( Palette::STD_GRAY );
 
-    Color cfill   {255, 20, 10};
+    Color cfill   {50, 20, 10};
     Color cstroke {5, 5, 10};
 
     canvas.setStrokeColor(cstroke);
@@ -55,20 +60,46 @@ int main() {
         bottom, top, face1, face2
     };
 
+    int radius = 0;
+    int dx = 1, dy = 1;
+    Graphics::Point2D rect {2, 2};
+
     while ( true ) {
         canvas.clear();
+        // cube
         canvas.restore();
-        canvas.text(1, 1, "3D Hello world!");
-        canvas.moveTo(width / 2 , height / 2 );
-
+        canvas.text(3, 2, "3D Test");
+        canvas.moveTo(10, 20);
         // rotation test
         double angle = 0.1;
-        int scale = 12;
-        animateFace('z', angle, scale, canvas, cube);
+        int scale = 15;
         animateFace('x', angle, scale, canvas, cube);
+        animateFace('z', angle, scale, canvas, cube);
+
+        // circle
+        canvas.restore();
+        canvas.moveTo(3 * width  / 4, height / 2);
+        canvas.text(-10, -height/ 2 + 2, "2D Test");
+
+        canvas.strokeCircle(0, 0, radius++);
+        radius %= 20;
+
+        canvas.plot2d(&func_def, {-width / 4, width / 4});
+        deltax++;
+
+        // rect
+        canvas.restore();
+        canvas.fillRect(rect.x, rect.y, 5, 5);
+
+        if ( rect.x + 5 >= width || rect.x <= 0 ) {
+            dx *= -1;
+        }
+        if ( rect.y + 5 >= height || rect.y <= 0 ) {
+            dy *= -1;
+        }
+        rect.x += dx; rect.y += dy;
 
         canvas.draw();
-
         std::this_thread::sleep_for(std::chrono::milliseconds(1000 / 24));
     }
 }
